c7081e4b6bfe728373348806c8a95d9a22dba9ae
mohammaddebsawi <mohammadhmada@gmail.com>
2024-08-17 03:21:00 +0300

    Set up device compatibility

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,18 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     header.classList.toggle("sticky",window.scrollY > 150);
 })
+
+// Event listener for dropdown menu
+
+const menu = document.querySelector("#menu-icon");
+const navlinks = document.querySelector(".navlinks");
+
+menu.onclick = ()=>{
+    menu.classList.toggle("bx-x");
+    navlinks.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menu.classList.remove("bx-x");
+    navlinks.classList.remove("open");
+}
